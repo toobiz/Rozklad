8434e70c866c885796f3d0a099c7adb38b74dfac
Mike Tubis <mike.tubis@gmail.com>
2017-06-28 23:34:27 +0200

    adjust SearchBar

--- Rozklad/PickerViewController.swift
+++ Rozklad/PickerViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class PickerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class PickerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchString: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.isHidden = true
+        spinner.isHidden = true
+        searchBar.becomeFirstResponder()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        searchBar.resignFirstResponder()
     }
+    
+    // MARK: TableView delegate
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
@@ -30,11 +41,38 @@ class PickerViewController: UIViewController, UITableViewDelegate, UITableViewDa
         tableView.register(UINib(nibName: "StationCell", bundle: nil), forCellReuseIdentifier: "StationCell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "StationCell", for: indexPath) as! StationCell
         
-        cell.stationName.text = "Stacja"
+        cell.stationName.text = searchString
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("TableView tapped")
+        view.endEditing(true)
+        dismiss(animated: true) {
+            print("Picker closed")
+        }
+    }
     
+    // MARK: SearchBar delegate
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("Search button clicked")
+        searchBar.resignFirstResponder()
+    }
 
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        searchString = searchText
+        tableView.reloadData()
+        search(searchText)
+    }
+    
+    func search(_: String) {
+        tableView.isHidden = false
+    }
 }
+
+
+
+
